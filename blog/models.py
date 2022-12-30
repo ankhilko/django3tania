d58d1a299c93b2d05527bf79413cd322a26ad83d
sanr <fieldstreet.27@gmail.com>
2022-12-30 20:27:29 +0300

    starting adding templates

--- blog/models.py
+++ blog/models.py
@@ -21,24 +21,6 @@ class Subject(models.Model):
         verbose_name_plural = 'subjects'
 
 
-class City(models.Model):
-    name = models.CharField(
-        max_length=100,
-        unique=True,
-        blank=False,
-        null=False,
-        primary_key=True,
-    )
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = 'blog_cities'
-        verbose_name = 'city'
-        verbose_name_plural = 'cities'
-
-
 class Country(models.Model):
     name = models.CharField(
         max_length=100,
@@ -55,6 +37,25 @@ class Country(models.Model):
         db_table = 'blog_countries'
         verbose_name = 'country'
         verbose_name_plural = 'countries'
+
+
+class City(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        blank=False,
+        null=False,
+        primary_key=True,
+    )
+    country = models.ForeignKey(Country, null=False, blank=False, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'blog_cities'
+        verbose_name = 'city'
+        verbose_name_plural = 'cities'
 
 
 class EducationalInstitution(models.Model):
@@ -120,12 +121,19 @@ class People(models.Model):
         blank=True,
         null=True,
     )
+    mobile_number = models.CharField(
+        max_length=20,
+        verbose_name='mobile number',
+        unique=True,
+        blank=True,
+        null=True,
+    )
 
     city = models.ForeignKey(City, null=True, blank=True, on_delete=models.DO_NOTHING)
     country = models.ForeignKey(Country, null=True, blank=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return self.family_name + ' ' + self.first_name
+        return self.family_name + ', ' + self.first_name
 
 
 class Teacher(People):
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,41 @@
+from datetime import datetime
+
 from django.shortcuts import render
-from blog.models import *
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.views.generic import TemplateView, ListView
+from django.views import View
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from blog.models import Subject, Student, Teacher
 # Create your views here.
 
 
+class IndexTemplateView(TemplateView):
+    template_name = 'blog/index.html'
+
+
+class SubjectListView(ListView):
+    template_name = 'blog/subjects.html'
+    model = Subject
+    context_object_name = 'subjects'
+
+
+class StudentListView(ListView):
+    template_name = 'blog/students.html'
+    model = Student
+    context_object_name = 'students'
+
+
+class TeacherListView(ListView):
+    template_name = 'blog/teachers.html'
+    model = Teacher
+    context_object_name = 'teachers'
+
+
+
+# class ProductListView(LoginRequiredMixin, ListView):
+#     login_url = '/signin/'
+#     template_name = 'main/products.html'
+#     model = Product
+#     context_object_name = 'products'
