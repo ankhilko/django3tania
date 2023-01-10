25d137ead6125ca02d76ea37c483f64c1c26e529
sanr <fieldstreet.27@gmail.com>
2023-01-10 10:10:47 +0300

    template changed again

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from registration.views import SignInView, ProfileView, LogoutView, RegisterCreateView
+
 from .views import SubjectListView, IndexTemplateView, StudentListView, TeacherListView, PersonListView
 
 urlpatterns = [
@@ -7,5 +9,8 @@ urlpatterns = [
     path('students/', StudentListView.as_view(), name='students'),
     path('teachers/', TeacherListView.as_view(), name='teachers'),
     path('userprofile/', PersonListView.as_view(), name='userprofile'),
+    path('registration/signin/', SignInView.as_view(), name='signin'),
+    path('registration/signup/', RegisterCreateView.as_view(), name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
 ]
